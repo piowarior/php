142ae2eee8a1e2c1fc66d9e3b25e8e6ef5ad6ae5
piowarior <muhamadrohisuliman@gmail.com>
2025-11-16 23:39:54 +0700

    update new 16-11-2025

--- laravel/laravel-pemula/pert2/routes/web.php
+++ laravel/laravel-pemula/pert2/routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::resource('posts', PostController::class);
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', function () {
+    return view('posts.startup');
+});
+
+Route::get('/posts', [PostController::class, 'index']);
