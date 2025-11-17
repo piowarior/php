96b1d676694397cac37204a54e8703bd88096dd9
piowarior <muhamadrohisuliman@gmail.com>
2025-11-17 13:54:14 +0700

    update new 17-11-2025

--- laravel/laravel-pemula/pert2/resources/views/posts/startup.blade.php
+++ laravel/laravel-pemula/pert2/resources/views/posts/startup.blade.php
@@ -3,11 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="asset/global.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>web post</title>
 </head>
 <body>
+    <header class="bagian kepala">
+        <ul>
+            <li>
+                <p>15 / November / 2025</p>
+            </li>
+            <li>
+                <p>piowarior</p>
+            </li>
+        </ul>
+    </header>
 
-    <p>hello word</p>
-    
+    <main class="bagian utama">
+        <div class="utama-kiri">
+            <svg width="1" height="98" viewBox="0 0 1 98" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0.5" y1="2.18557e-08" x2="0.499996" y2="98" stroke="black"/>
+            </svg>
+            <p>BE HUMBLE</p>
+
+        </div>
+        <div class="utama-kanan">
+            <h1>BLOG SPHERE</h1>
+            <img src="/allbuild/post/asset/gambar/gambar cewek2.png" alt="">
+            <img src="/allbuild/post/asset/gambar/bayangan.png" alt="">
+        </div>
+    </main>
+
+    <section class="bagian tengah 1">
+
+    </section>
+
+    <script src="global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
 </body>
 </html>
